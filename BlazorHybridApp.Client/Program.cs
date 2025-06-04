a52d56331351fe3793f92cfe6f77646306fae1b3
Yasuaki Kudo <yasu@yasuaki.com>
2025-06-04 15:16:12 +0900

    Add Counter2 component using custom element

--- BlazorHybridApp.Client/Program.cs
+++ BlazorHybridApp.Client/Program.cs
@@ -1,10 +1,15 @@
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorHybridApp.Client.Pages;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthenticationStateDeserialization();
+
+// Register Counter component as custom element <my-counter>
+builder.RootComponents.RegisterCustomElement<Counter>("my-counter");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
